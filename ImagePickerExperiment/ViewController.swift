7452bbd160edc9b490aea6d1321c7a4dbdd8f93e
Vinicius Brito <vibrito@gmail.com>
2015-09-02 00:06:04 -0300

    Initial Commit

---
+++ ImagePickerExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePickerExperiment
+//
+//  Created by Vinicius Brito on 9/2/15.
+//  Copyright (c) 2015 Vinicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
